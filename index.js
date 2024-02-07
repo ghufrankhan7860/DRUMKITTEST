22555b5d980d8e7be2673ffcdf45ead5d4c36c9a
Coder Gian <59571105+ghufrankhan7860@users.noreply.github.com>
2024-02-07 16:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var drumCounts=document.querySelectorAll(".drum").length;
-var path="sounds/";
+
 var sounds=["crash", "kick-bass", "snare", "tom-1", "tom-2", "tom-3", "tom-4"];
 
 // for (let i = 0; i < drumCounts ; i++) {
@@ -28,35 +28,35 @@ document.addEventListener("keydown", function(event){
     var audioToPlay;
     switch (keyPressed) {
         case "w":
-            audioToPlay=new Audio("sounds/crash.mp3");
+            audioToPlay=new Audio("crash.mp3");
             audioToPlay.play();
             break;
         case "a":
-            audioToPlay=new Audio("sounds/kick-bass.mp3");
+            audioToPlay=new Audio("kick-bass.mp3");
             audioToPlay.play();
             break;
         case "s":
-            audioToPlay=new Audio("sounds/snare.mp3");
+            audioToPlay=new Audio("snare.mp3");
             audioToPlay.play();
             break;
         case "d":
-            audioToPlay=new Audio("sounds/tom-1.mp3");
+            audioToPlay=new Audio("tom-1.mp3");
             audioToPlay.play();
             break;
         case "j":
-            audioToPlay=new Audio("sounds/tom-2.mp3");
+            audioToPlay=new Audio("tom-2.mp3");
             audioToPlay.play();
             break;
         case "k":
-            audioToPlay=new Audio("sounds/tom-3.mp3");
+            audioToPlay=new Audio("tom-3.mp3");
             audioToPlay.play();
             break;
         case "l":
-            audioToPlay=new Audio("sounds/tom-4.mp3");
+            audioToPlay=new Audio("tom-4.mp3");
             audioToPlay.play();
             break;
         default:
-            audioToPlay=new Audio("sounds/crash.mp3");
+            audioToPlay=new Audio("crash.mp3");
             audioToPlay.play();
             break;
 
